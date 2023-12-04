edc1e51c65efe498f389e006e719108d14341172
nik132-eng <nikunjrohit10@gmail.com>
2023-12-05 00:33:49 +0530

    solved two easy

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,31 @@
 */
 
 function isAnagram(str1, str2) {
+  const countchar = (str) => {
+    const charcount = {};
+    for (let char of str){
+      char = char.toLowerCase();
+      if (char !== ' ') { 
+        charcount[char] = (charcount[char] || 0) + 1
+      }
+    }
+    return charcount;
+  }
 
+  const charCount1 = countchar(str1);
+  const charCount2 = countchar(str2);
+
+  if(Object.keys(charCount1).length != Object.keys(charCount2).length){
+    return false;
+  }
+
+  for (let char in charCount1) {
+    if (charCount1[char] !== charCount2[char]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isAnagram;
