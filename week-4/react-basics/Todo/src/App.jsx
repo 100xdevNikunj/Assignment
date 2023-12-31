d182b9af98af604e836af47effb7505d82dad364
nik132-eng <nikunjrohit10@gmail.com>
2023-12-31 17:13:50 +0530

    make  todo app

--- week-4/react-basics/Todo/src/App.jsx
+++ week-4/react-basics/Todo/src/App.jsx
@@ -1,18 +1,55 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const addTodo = () => {
+    if (title.trim() && description.trim()) {
+      setTodos(prevTodos => [
+        ...prevTodos,
+        { title, description }
+      ]);
+      setTitle('');
+      setDescription('');
+    }
+  };
 
   return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="container">
+      <h1>TODO LIST</h1>
+      <div className="card input-section">
+        <input
+          type='text'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+          placeholder="Enter title"
+        />
+        <br />
+        <input
+          type='text'
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+          placeholder="Enter description"
+        />
+        <button onClick={addTodo}>ADD TODO</button>
       </div>
-    </>
-  )
+      <div className="card todo-list">
+        {todos.map((todo, index) => (
+          <div key={index} className="todo-card">
+            <label>Title</label>
+            <h4>{todo.title}</h4>
+            <label>Description</label>
+            <p>{todo.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
