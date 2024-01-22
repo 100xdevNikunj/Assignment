9b1e1643533cabca223e74e6d31cca364ccebc1c
nik132-eng <nikunjrohit10@gmail.com>
2024-01-22 08:47:29 +0530

    otp assignment finish

--- week-7/Practice/src/pages/components/otpInput.jsx
+++ week-7/Practice/src/pages/components/otpInput.jsx
@@ -12,21 +12,57 @@ const otpInput = ({ length, onSubmit }) => {
       otpInputRefs[0].current.focus();
     }
   }, []);
-
+  
   function handleChange(index, e) {
     const newOtp = [...otp];
-    newOtp[index] = e.target.value;
+    const lastStringCharacter = e.target.value.slice(-1);
+    const isNumber = /^\d+$/.test(lastStringCharacter);
+    
+    // If it's a number, set lastNumber
+    const lastNumber = isNumber ? lastStringCharacter : null;
+  
+    newOtp[index] = lastNumber;
     setOtp(newOtp);
-  }
+  
+    // Move to the next input field if a number is entered
+    if (isNumber && index < length - 1) {
+      otpInputRefs[index + 1].current.focus();
+    }
+
+    const combineOTP = newOtp.join("");
+    if(combineOTP.length==length) onSubmit(combineOTP)
+  }  
   
 
-  function handleClick(index){
-
+  function handleClick(index) {
+    if (otpInputRefs[index].current) {
+      const input = otpInputRefs[index].current;
+      const length = input.value.length;
+      input.setSelectionRange(length, length);
+      input.focus();
+    }
+  }
+  
+  function handleKeyUp(index, e) {
+    if (e.key === "Backspace") {
+      const newOtp = [...otp];
+      newOtp[index] = "";
+      setOtp(newOtp);
+  
+      if (index > 0) {
+        otpInputRefs[index - 1].current.focus();
+      }
+    }
   }
 
-  function handleKeyDown(index, e){
-
+  function handleKeyDown(index, e) {
+    // Prevent non-numeric input
+    if (e.key.length === 1 && !/^\d+$/.test(e.key)) {
+      e.preventDefault();
+      alert("Please enter a number");
+    } 
   }
+  
   return (
     <div>
       {otp.map((value, index) => {
@@ -38,6 +74,7 @@ const otpInput = ({ length, onSubmit }) => {
                 onChange={(e) => handleChange(index, e)}
                 onClick={()=> handleClick(index)}
                 onKeyDown={(e) => handleKeyDown(index, e)}
+                onKeyUp={(e) => handleKeyUp(index, e)}
                 className="otpinput"
                 ref={otpInputRefs[index]}
             />
