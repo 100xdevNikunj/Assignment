0e10fb3280410cf367712c6c63263008e534c4da
nik132-eng <nikunjrohit10@gmail.com>
2024-03-10 17:16:57 +0530

    complete week 14 assignment

---
+++ week-14/week-14-assignment/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+  return (
+    <nav className='flex gap-20 justify-center my-20'>
+        <Link className="border-2 border-gray-400 px-4 py-2 rounded-xl" href="/">Home</Link>
+        <Link className="border-2 border-gray-400 px-4 py-2 rounded-xl" href="static-page">Server Page</Link>
+        <Link className="border-2 border-gray-400 px-4 py-2 rounded-xl" href="interactive-page">Client Page</Link>      
+    </nav>
+  );
+};
+
+export default Navbar;
